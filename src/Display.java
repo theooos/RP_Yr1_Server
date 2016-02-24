7bd6220b890adb08ceded0ccbecdb949a4c7157e
axk580 <axk580@cs.bham.ac.uk>
2016-02-24 12:35:18 +0000

    Added Display

---
+++ src/Display.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+public class Display extends JFrame
+{
+	public Display()
+	{
+		super("Warehouse Interface");
+	}
+
+	public static void main(String[] args)
+	{
+		new Display();
+	}
+}
