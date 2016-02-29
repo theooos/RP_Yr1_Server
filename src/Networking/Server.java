79e8c0e022db436e2c34f2f4279bbbb29aba7967
theooos <tjmg@outlook.com>
2016-02-29 10:52:42 +0000

    Begins JUnit test work.

--- src/Networking/Server.java
+++ src/Networking/Server.java
@@ -2,8 +2,31 @@ package Networking;
 
 public class Server extends Thread {
 	
+	private boolean isAlive = true;
+	
 	public Server() {
 		// TODO Auto-generated constructor stub
 	}
 	
+	@Override
+	public void run(){
+		while(isAlive){
+			try {
+				Thread.sleep(100);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	
+	public void send(String comm){
+		// TODO The sending bit.
+		System.out.println(comm);
+	}
+	
+	public boolean serverAlive(){
+		return isAlive;
+	}
 }
