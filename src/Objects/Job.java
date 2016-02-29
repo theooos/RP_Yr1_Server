36ced2899e7b1e8e0695ab9c7a5e5c1beec600e8
George Sabourin <gcs516@student.bham.ac.uk>
2016-02-29 10:48:53 +0000

    Added a few more objects.

--- src/Objects/Job.java
+++ src/Objects/Job.java
@@ -1,60 +1,101 @@
 package Objects;
 
-import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Represents a job to be carried out.
  */
-public class Job implements Serializable {
+public class Job {
 
-    public List<Job> currentJobs = new ArrayList<Job>();
+	public static Map<Integer, Job> currentJobs = new HashMap<Integer, Job>();
 
-    private int jobID;
-    private List<SingleTask> tasks;
-    
-    /**
-     * Create an empty job with an ID.
-     * @param jobID The job ID.
-     */
-    public Job(int jobID) {
-        this.jobID = jobID;
-        this.tasks = new ArrayList<SingleTask>();
-    }
+	private List<Task> tasks;
 
-    /**
-     * Create a job from a job ID and job items.
-     * @param jobID The job ID.
-     * @param items The items in the job.
-     */
-    public Job(int jobID, List<SingleTask> tasks) {
-        this.jobID = jobID;
-        this.tasks = tasks;
-    }
-
-    /**
-     * Add an item to the list of job items.
-     * @param item The job item to be added.
-     */
-    public void addItem(SingleTask task) {
-        tasks.add(task);
-    }
-
-    /**
-     * Add an item to the list of job items.
-     * @param item The item to be added.
-     * @param qty The amount of the item needed.
-     */
-    public void addItem(Item item, int qty) {
-        tasks.add(new SingleTask(item, qty));
-    }
-
-    // toString method for debugging purposes
-	@Override
-	public String toString() {
-		return "Job [jobID=" + jobID + ", tasks=" + tasks + "]";
+	/**
+	 * Create an empty job.
+	 */
+	public Job() {
+		this.tasks = new ArrayList<Task>();
 	}
 
-    
+	/**
+	 * Create a job from a list of tasks.
+	 * @param tasks The tasks in the job.
+	 */
+	public Job(List<Task> tasks) {
+		this.tasks = tasks;
+
+		rewardPerItem();
+		rewardPerWeight();
+	}
+
+	/**
+	 * Add an item to the list of job items.
+	 * @param item The job item to be added.
+	 */
+	public void addTask(Task t) {
+		tasks.add(t);
+
+		rewardPerItem();
+		rewardPerWeight();
+	}
+
+	/**
+	 * Add an item to the list of job items.
+	 * @param item The item to be added.
+	 * @param qty The amount of the item needed.
+	 */
+	public void addTask(Item item, int qty) {
+		tasks.add(new Task(item, qty));
+
+		rewardPerItem();
+		rewardPerWeight();
+	}
+
+	/**
+	 * Calculate the reward for this job per item.
+	 * @return The reward per item.
+	 */
+	public double rewardPerItem() {
+
+		int numOfItems = 0;
+		double reward = 0f;
+
+		for(int i = 0; i < tasks.size(); i++) {
+			Item item = tasks.get(i).getItem();
+			numOfItems += tasks.get(i).getQuantity();
+			reward += item.getReward() * tasks.get(i).getQuantity(); 
+		}
+
+		return (reward / (double) numOfItems);
+
+	}
+
+	/**
+	 * Calculate the reward for this job per weight.
+	 * @return The reward per weight.
+	 */
+	public double rewardPerWeight() {
+
+		double reward = 0f;
+		double weight = 0f;
+
+		for(int i = 0; i < tasks.size(); i++) {
+			Item item = tasks.get(i).getItem();
+			reward += item.getReward() * tasks.get(i).getQuantity();
+			weight += item.getWeight() * tasks.get(i).getQuantity();
+		}
+
+		return (reward / weight);
+	}
+
+	// toString method for debugging purposes
+	@Override
+	public String toString() {
+		return "Job [tasks=" + tasks + "]";
+	}   
+
 }
