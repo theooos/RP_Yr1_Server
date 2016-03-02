c0e33a46f4d49922215c2c16b61d8f10ed80f406
kowalskidev <skowalskidev@gmail.com>
2016-03-02 14:18:12 +0000

    deleting my own mistake upload

--- src/dataStructures/GeneralProtocol.java
+++
@@ -1,8 +0,0 @@
-package dataStructures;
-
-public class GeneralProtocol {
-	public static final int NORTH = 0;
-	public static final int SOUTH = 1;
-	public static final int EAST = 2;
-	public static final int WEST = 3;
-}
