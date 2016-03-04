4278dd3ba3ab56ed383febf918e80bda3312ff32
Theo Gregory <tjmg@outlook.com>
2016-03-04 11:21:47 +0000

    Adds AllRobots so all the alive robots can be statically and synchronously accessed.

---
+++ src/Objects/AllRobots.java
@@ -0,0 +1,66 @@
+package Objects;
+
+import java.util.ArrayList;
+import java.util.UUID;
+
+public class AllRobots {
+
+	private static ArrayList<RobotInfo> robots = new ArrayList<RobotInfo>();
+	
+	public synchronized static ArrayList<RobotInfo> getAllRobots(){
+		return robots;
+	}
+	
+	public synchronized static RobotInfo getRobot(UUID name){
+		for(RobotInfo robot : robots){
+			if(robot.getName().equals(name)) return robot;
+		}
+		return null;
+	}
+
+	public synchronized static void addRobot(RobotInfo newBot){
+		robots.add(newBot);
+	}
+	
+	public synchronized static boolean checkExists(UUID name){
+		for(RobotInfo robot : robots){
+			if(robot.getName().equals(name)) return true;
+		}
+		return false;
+	}
+	
+	public synchronized static void modifyRobot(UUID name, RobotInfo newInfo){
+		ArrayList<RobotInfo> newList = new ArrayList<RobotInfo>();
+		boolean robotFound = false;
+		
+		for(RobotInfo robot : robots){
+			if(robot.getName().equals(name)){
+				newList.add(newInfo);
+				robotFound = true;
+			}
+			else{
+				newList.add(robot);
+			}
+		}
+		
+		robots = newList;
+		if(!robotFound) throw new IllegalArgumentException("Robot " + name + " not found");
+	}
+	
+	public synchronized static void removeRobot(UUID name){
+		ArrayList<RobotInfo> newList = new ArrayList<RobotInfo>();
+		boolean robotFound = false;
+		
+		for(RobotInfo robot : robots){
+			if(robot.getName().equals(name)){
+				robotFound = true;
+			}
+			else{
+				newList.add(robot);
+			}
+		}
+		
+		robots = newList;
+		if(!robotFound) throw new IllegalArgumentException("Robot " + name + " not found");
+	}
+}
