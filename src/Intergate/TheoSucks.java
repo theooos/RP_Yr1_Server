e854f724e9419feb6b0b5c353ebb7749d09056a0
George Sabourin <gcs516@student.bham.ac.uk>
2016-03-07 21:44:38 +0000

    Updates

--- src/Intergate/TheoSucks.java
+++ src/Intergate/TheoSucks.java
@@ -1,7 +1,16 @@
 package Intergate;
 
+import java.awt.GridLayout;
+
+import javax.swing.JFrame;
+import javax.swing.WindowConstants;
+
 import JobInput.JobProcessor;
 import Networking.Server;
+import warehouseInterface.GridMap;
+import warehouseInterface.JobTable;
+import warehouseInterface.RobotTable;
+import warehouseInterface.Statistics;
 
 public class TheoSucks {
 
@@ -11,13 +20,28 @@ public class TheoSucks {
         JobProcessor.processItemFiles("res/items.csv", "res/locations.csv");
         JobProcessor.processJobFiles("res/jobs.csv", "res/cancellations.csv");
         
-        // FRAN'S STUFF MAY GO HERE WHEN IT WORKS, BUT IDK BECAUSE SHE SUCKS
+        // FRAN'S STUFF MAY GO HERE WHEN IT WORKS, BUT IDK BECAUSE SHE SUCKS, THEN AGAIN: THEO SUCKS MORE <3xxx
         
         ////
         
         // Theo --- Server: Starting the server
         Server server = new Server();
 		
+		JFrame frame = new JFrame("Warehouse Interface");
+		frame.setLayout(new GridLayout(2, 2));
+		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		frame.setResizable(false);
+		frame.setSize(800, 600);
+
+		frame.add(JobTable.draw());
+		frame.add(GridMap.createGrid());
+		frame.add(RobotTable.draw());
+		frame.add(Statistics.draw());
+		frame.setVisible(true);
+		
+		
+        
+        
 	}
 
 }
