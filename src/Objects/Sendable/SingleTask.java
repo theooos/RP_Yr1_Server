8b0e24ba284f5eb0ce61a1829a81cfe8e651daa6
bxh538 <bxh538@student.bham.ac.uk>
2016-03-09 12:19:36 +0000

    SingleTask update

--- src/Objects/Sendable/SingleTask.java
+++ src/Objects/Sendable/SingleTask.java
@@ -1,5 +1,6 @@
 package Objects.Sendable;
 
+import java.awt.Point;
 
 /**
  * SHARED OBJECTS
@@ -9,16 +10,18 @@ public class SingleTask implements SendableObject {
 	
 	private String itemID;
 	private int quantity;
+    private Point location;
 
     /**
      * Create a new single task.
      * @param itemID The item id.
      * @param quantity The quantity of the item.
      */
-	public SingleTask(String itemID, int quantity) {
+	public SingleTask(String itemID, int quantity, Point location) {
 		
 		this.itemID = itemID;
 		this.quantity = quantity;
+        this.location = location;
 		
 	}
 	
@@ -36,6 +39,14 @@ public class SingleTask implements SendableObject {
 	 */
 	public int getQuantity() {
 		return quantity;
+	}
+
+	/**
+	 * Get the location.
+	 * @return The location.
+	 */
+	public int getLocation() {
+		return location;
 	}
 
 	// toString method for debugging purposes
