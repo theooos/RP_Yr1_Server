c7242bb3c0a94583394e6705d5232e3dbba1014f
axk580 <axk580@cs.bham.ac.uk>
2016-03-09 12:45:15 +0000

    Deleting Objects...

--- src/Objects/Direction.java
+++
@@ -1,6 +0,0 @@
-package Objects;
-
-public enum Direction
-{
-	NORTH, SOUTH, EAST, WEST
-}
--- src/Objects/Sendable/SendableObject.java
+++
@@ -1,5 +0,0 @@
-package Objects.Sendable;
-
-public interface SendableObject {
-	public String parameters();
-}
