201a3ceabbd6af23bc13c9ca251c2b25251dd90e
bxh538 <bxh538@student.bham.ac.uk>
2016-03-14 10:04:08 +0000

    Deciding on features

---
+++ src/JobSelection/Features.java
@@ -0,0 +1,46 @@
+public class Features {
+
+    public static class NumOfTasks {
+        
+        public static int 1TO2 = 0;
+        public static int 3TO4 = 1;
+        public static int 5PLUS = 2;
+
+    }
+
+    public static class TotalItems {
+
+        public static int 1TO4 = 3;
+        public static int 5TO8 = 4;
+        public static int 9TO12 = 5;
+        public static int 14PLUS = 6;
+
+    }
+
+    public static class HighestRewardItem {
+        
+        public static int 0TO5 = 7;
+        public static int 6TO10 = 8;
+        public static int 11TO15 = 9;
+        public static int 16PLUS = 10;
+
+    }
+
+    public static class HeaviestItem {
+
+        public static int 0TO1 = 11;
+        public static int 2TO3 = 12;
+        public static int 4PLUS = 13;
+
+    }
+
+    public static int HighestQuantity {
+
+        public static int 1TO2 = 14;
+        public static int 3TO4 = 15;
+        public static int 5TO6 = 16;
+        public static int 7PLUS = 17;
+
+    }
+
+}
