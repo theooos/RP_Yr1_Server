86015f3934c9a2e559205d1fd8cd9d7112088e81
Fran <fxl506@student.bham.ac.uk>
2016-03-14 10:09:16 +0000

    no numbers - just words

--- src/JobSelection/Features.java
+++ src/JobSelection/Features.java
@@ -2,44 +2,44 @@ public class Features {
 
     public static class NumOfTasks {
         
-        public static int 1TO2 = 0;
-        public static int 3TO4 = 1;
-        public static int 5PLUS = 2;
+        public static int ONETOTWO = 0;
+        public static int THREETOFOUR = 1;
+        public static int FIVEPLUS = 2;
 
     }
 
     public static class TotalItems {
 
-        public static int 1TO4 = 3;
-        public static int 5TO8 = 4;
-        public static int 9TO12 = 5;
-        public static int 14PLUS = 6;
+        public static int ONETOFOUR = 3;
+        public static int FIVETOEIGHT = 4;
+        public static int NINETOTWELVE = 5;
+        public static int FOURTEENPLUS = 6;
 
     }
 
     public static class HighestRewardItem {
         
-        public static int 0TO5 = 7;
-        public static int 6TO10 = 8;
-        public static int 11TO15 = 9;
-        public static int 16PLUS = 10;
+        public static int ZEROTOFIVE = 7;
+        public static int SIXTOTEN = 8;
+        public static int ELEVENTOFIFTEEN = 9;
+        public static int SIXTEENPLUS = 10;
 
     }
 
     public static class HeaviestItem {
 
-        public static int 0TO1 = 11;
-        public static int 2TO3 = 12;
-        public static int 4PLUS = 13;
+        public static int ZEROTOONE = 11;
+        public static int TWOTOTHREE = 12;
+        public static int FOURPLUS = 13;
 
     }
 
-    public static int HighestQuantity {
+    public static class HighestQuantity {
 
-        public static int 1TO2 = 14;
-        public static int 3TO4 = 15;
-        public static int 5TO6 = 16;
-        public static int 7PLUS = 17;
+        public static int ONETOTWO = 14;
+        public static int THREETOFOUR = 15;
+        public static int FIVETOSIX = 16;
+        public static int SEVENPLUS = 17;
 
     }
 
