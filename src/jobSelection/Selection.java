fd7ac9bd0abe81d777d42290fe978c9d5b27181b
George Sabourin <gcs516@student.bham.ac.uk>
2016-03-17 15:57:46 +0000

    Debug code in Selection

--- src/jobSelection/Selection.java
+++ src/jobSelection/Selection.java
@@ -14,11 +14,18 @@ public class Selection {
 		@Override
 		public int compare(Job job1, Job job2) {
 
+			/*
             Integer valueJob1 = (int) ((job1.rewardPerItem() * 100 + job1.rewardPerDistance() * 100) / 2);
             Integer valueJob2 = (int) ((job2.rewardPerItem() * 100 + job2.rewardPerDistance() * 100) / 2);
+			*/
 			
-            return 1;
+			
+			// DEBUGGING CODE
+			Integer valueJob1 = (int) job1.rewardPerItem();
+			Integer valueJob2 = (int) job2.rewardPerItem();
+            
 	
+            return 1;
 		}
 	});
 	
