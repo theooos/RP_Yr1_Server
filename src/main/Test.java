049936dba0e8d7d5fb617f9289b21ed1f0d6803c
bxh538 <bxh538@student.bham.ac.uk>
2016-03-17 16:42:04 +0000

    Added Test file for Maria

---
+++ src/main/Test.java
@@ -0,0 +1,31 @@
+package main;
+import java.util.PriorityQueue;
+
+import Objects.Job;
+import Objects.WarehouseMap;
+import jobInput.JobProcessor;
+import jobSelection.Selection;
+
+public class Test {
+	
+	public static WarehouseMap map;
+
+    public static void main(String[] args) {
+
+		JobProcessor.processItemFiles("res/items.csv", "res/locations.csv");
+		JobProcessor.processJobFiles("res/jobs.csv", "res/cancellations.csv");
+
+        map = new WarehouseMap("res/drops.csv");
+
+        PriorityQueue<Job> q = Selection.createQueue();
+        Job firstJob = q.remove();
+        System.out.println(firstJob.getTotalReward());
+        firstJob = q.remove();
+        System.out.println(firstJob.getTotalReward());
+        firstJob = q.remove();
+        System.out.println(firstJob.getTotalReward());
+        
+        
+    }
+
+}
